d8548b0c3deec596dbf2aa8bd652b530cf4cdfdc
Josefina Husso <husso.josefina@gmail.com>
2023-04-07 14:43:26 +0300

    changed dup2 order

--- children.c
+++ children.c
@@ -6,7 +6,7 @@
 /*   By: jhusso <jhusso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 14:13:14 by jhusso            #+#    #+#             */
-/*   Updated: 2023/04/07 12:45:59 by jhusso           ###   ########.fr       */
+/*   Updated: 2023/04/07 14:32:15 by jhusso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,10 @@ void	first_child(t_pipex *pipex, char **envp, char **av)
 	pipex->cmd1_path = get_cmd_path(*pipex->cmd1, pipex->split_path);
 	if (pipex->cmd1_path == NULL)
 		error_msg("command not found", *pipex->cmd1, 1);
-	if (dup2(pipex->fd[1], STDOUT_FILENO) == -1)
-		error("Error", 1);
 	close(pipex->fd[0]);
 	if (dup2(pipex->infile, STDIN_FILENO) == -1)
+		error("Error", 1);
+	if (dup2(pipex->fd[1], STDOUT_FILENO) == -1)
 		error("Error", 1);
 	if (pipex->infile != -1)
 	{
@@ -47,11 +47,11 @@ void	second_child(t_pipex *pipex, char **envp, char **av)
 	pipex->cmd2_path = get_cmd_path(*pipex->cmd2, pipex->split_path);
 	if (pipex->cmd2_path == NULL)
 		error_msg("command not found", *pipex->cmd2, 1);
-	if (dup2(pipex->fd[0], STDIN_FILENO) == -1)
-		error("Error 3", 1);
 	close(pipex->fd[1]);
 	if (dup2(pipex->outfile, STDOUT_FILENO) == -1)
 		error("Error 4", 1);
+	if (dup2(pipex->fd[0], STDIN_FILENO) == -1)
+		error("Error 3", 1);
 	if (pipex->outfile != -1)
 	{
 		if ((execve(pipex->cmd2_path, pipex->cmd2, envp) == -1))
